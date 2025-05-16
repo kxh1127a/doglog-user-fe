af625c46eeeea736bec931ece1252998bdba7a9f
yoondd <yoondd@kakao.com>
2025-05-16 17:50:47 +0900

    design: nav-desgin

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,11 +3,12 @@ import styles from '@/styles/Nav.module.scss'
 import Link from "next/link";
 import { FaHome } from "react-icons/fa";
 import { FaCalendarAlt } from "react-icons/fa";
-import { FaLightbulb } from "react-icons/fa";
-import { FaMapMarkedAlt } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
 import { MdOutlinePets } from "react-icons/md";
-import { FaChartLine } from "react-icons/fa6";
-import { MdOutlineDesignServices } from "react-icons/md";
+import { MdOutlineArticle } from "react-icons/md";
+import { FaBell } from "react-icons/fa";
+import { IoChatboxEllipses } from "react-icons/io5";
+import { HiBars3 } from "react-icons/hi2";
 
 const Nav = () => {
     return (
@@ -21,7 +22,19 @@ const Nav = () => {
                 <li>
                     <Link href="/" className={styles.menu}>
                         <FaHome />
-                        <span>Home</span>
+                        <span>Dashboard</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/member" className={styles.menu}>
+                        <FaUserAlt />
+                        <span>Member</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/pet" className={styles.menu}>
+                        <MdOutlinePets />
+                        <span>Pet</span>
                     </Link>
                 </li>
                 <li>
@@ -31,44 +44,28 @@ const Nav = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link href="/caretip" className={styles.menu}>
-                        <FaLightbulb />
-                        <span>Care Tip</span>
+                    <Link href="/content" className={styles.menu}>
+                        <MdOutlineArticle />
+                        <span>Content</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/place" className={styles.menu}>
-                        <FaMapMarkedAlt />
-                        <span>Place</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/petcard" className={styles.menu}>
-                        <MdOutlinePets />
-                        <span>Pet Card</span>
+                    <Link href="/notice" className={styles.menu}>
+                        <FaBell />
+                        <span>Notice</span>
                     </Link>
                 </li>
                 <li>
                     <Link href="/chart" className={styles.menu}>
-                        <FaChartLine />
-                        <span>Chart</span>
+                        <IoChatboxEllipses />
+                        <span>QnA</span>
                     </Link>
                 </li>
             </ul>
 
-            <div className={styles.navbanner}>
-                <p>
-                    회원가입을 하면<br/>
-                    우리집 댕댕이만의<br/>
-                    예쁜 <span>Pet Card</span>를<br/>
-                    만들 수 있어요
-                </p>
-                <div className={styles.bannericon}>
-                    <MdOutlineDesignServices/>
-                </div>
+            <div className={styles.menubar}>
+                <HiBars3 />
             </div>
-
-
         </div>
     );
 };
