b66a987ed2032eb5ab072eaac869b0b8e1906651
yoon <yoondd@kakao.com>
2025-06-14 22:28:58 +0900

    feat: 멤버 메뉴 컴포넌트 생성

---
+++ src/components/MemberMenu.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from "next/link";
+
+
+const MemberMenu = () => {
+    return (
+        <div>
+            <Link href="/login"></Link>
+        </div>
+    );
+};
+
+export default MemberMenu;
