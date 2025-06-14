fe5c6a4501509efa600b5aecdddfd243d25b22e8
yoon <yoondd@kakao.com>
2025-06-14 22:43:54 +0900

    feat:케어팁 검색창

--- src/pages/caretip.tsx
+++ src/pages/caretip.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import MemberMenu from "@/components/MemberMenu";
+import styles from "../styles/caretip.module.scss";
+import Link from 'next/link';
+import { IoSearch } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+
 
 const Caretip = () => {
     return (
         <div className="main-container">
             <MemberMenu />
+            <div className={styles.searchContainer}>
+                <div className={styles.searchBox}>
+                    <input type="text"/>
+                    <IoSearch />
+                </div>
+                <Link href="/likes">
+                    <FaRegHeart />
+                </Link>
+            </div>
         </div>
     );
 };
